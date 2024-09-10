faff70fab8c40ca328738043dc9c344d9fcc38bd
shahidrahim-js <shahidrahim.js@gmail.com>
2024-09-10 16:58:06 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import { ChangeEvent, useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [searchText, setSearchText] = useState('');
+  const [users, setUsers] = useState([]);
+  const apiUrl = 'https://dummyjson.com/users';
+
+  const getUsers = () => {
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => setUsers(data.users))
+      .catch((err) => console.log('err:', err));
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+
+  const searchedUsers = () => {
+    return (
+      users?.filter((user) => {
+        return user.firstName.toLowerCase().includes(searchText.toLowerCase());
+      }) || users
+    );
+  };
+
+  return (
+    <>
+      <div className="searchField">
+        <input type="text" value={searchText} onChange={handleChange} />
+      </div>
+      {!users?.length ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Sr No.</th>
+              <th>Name</th>
+              <th>Gender</th>
+              <th>Email</th>
+              <th>City</th>
+            </tr>
+          </thead>
+          <tbody>
+            {searchedUsers()?.map((user) => {
+              return (
+                <tr>
+                  <td>{user.id}</td>
+                  <td>{user.firstName + ' ' + user.lastName}</td>
+                  <td>{user.gender}</td>
+                  <td>{user.email}</td>
+                  <td>{user.address.city}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
+}
+
+export default App;
